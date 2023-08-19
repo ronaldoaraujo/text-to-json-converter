24dc17dfd6963c4331d325c9c28ac8864c48748b
Ronaldo de Sousa Araujo <contato@ronaldoaraujo.eti.br>
2023-08-19 18:22:07 -0300

    Add implementation for App

--- src/main/java/com/challenge/App.java
+++ src/main/java/com/challenge/App.java
@@ -1,5 +1,10 @@
 package com.challenge;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.challenge.services.InputParser;
 import com.challenge.services.InputParserFactory;
 import com.challenge.services.TextToJsonConverter;
@@ -7,16 +12,48 @@ import com.challenge.services.InputParserFactory.Format;
 
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java Application <file or directory path>");
+            System.exit(1);
+        }
+
+        String inputPath = args[0];
+        File inputFile = new File(inputPath);
+
+        if (!inputFile.exists()) {
+            System.err.println("File or directory not found: " + inputPath);
+            System.exit(1);
+        }
+
+        processFiles(inputFile);
+    }
+
+    private static void processFiles(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles((dir, name) -> name.endsWith(".txt"));
+            if (files != null) {
+                for (File f : files) {
+                    processFiles(f);
+                }
+            }
+        } else if (file.isFile()) {
+            processFile(file);
+        }
+    }
+
+    private static void processFile(File file) {
+        String filePath = file.getPath();
         InputParser parser = InputParserFactory.createParser(Format.TEXT);
 
-        TextToJsonConverter converter = new TextToJsonConverter(parser);
+        try {
+            TextToJsonConverter converter = new TextToJsonConverter(parser);
+            String json = converter.convertToJson(filePath);
 
-
-        String inputFilePath = "src/test/resources/data_1.txt";
-        String json = converter.convertToJson(inputFilePath);
-
-        System.out.println( json );
+            String outputPath = filePath.replace(".txt", ".json");
+            Files.write(Paths.get(outputPath), json.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/challenge/AppTest.java
+++ src/test/java/com/challenge/AppTest.java
@@ -2,19 +2,50 @@ package com.challenge;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.After;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private String testDirectory = "src/test/resources";
+
+    @After
+    public void cleanUp() throws IOException {
+        File directory = new File(testDirectory);
+        File[] jsonFiles = directory.listFiles((dir, name) -> name.endsWith(".json"));
+
+        if (jsonFiles != null) {
+            for (File jsonFile : jsonFiles) {
+                Files.delete(jsonFile.toPath());
+            }
+        }
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testConvertAllFilesInDirectoryToJson() {
+        App.main(new String[]{testDirectory});
+
+        File directory = new File(testDirectory);
+        File[] jsonFiles = directory.listFiles((dir, name) -> name.endsWith(".json"));
+
+        assertTrue(jsonFiles != null && jsonFiles.length > 0);
+
+        for (File jsonFile : jsonFiles) {
+            assertTrue(jsonFile.exists());
+        }
+    }
+    @Test
+    public void testConvertSpecificFileToJson() {
+        String inputFilePath = "src/test/resources/data_1.txt";
+
+        App.main(new String[]{inputFilePath});
+
+        File jsonFile = new File(inputFilePath.replace(".txt", ".json"));
+
+        assertTrue(jsonFile.exists());
     }
 }
