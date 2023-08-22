a303ed2868ba4ddb4b9d1f594056b09d1c5b1f3b
Ronaldo de Sousa Araujo <contato@ronaldoaraujo.eti.br>
2023-08-22 00:44:20 -0300

    Refactor models to use record classes

--- src/main/java/com/challenge/models/Order.java
+++ src/main/java/com/challenge/models/Order.java
@@ -1,80 +1,20 @@
 package com.challenge.models;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.challenge.serializers.DoubleSerializer;
 
-@JsonPropertyOrder({"order_id", "total", "date", "products"})
-public class Order {
-
-    @JsonProperty("order_id")
-    private int id;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate date;
-    private List<Product> products;
-
-    public Order() {
-    }
-
-    public Order(int id, LocalDate date, List<Product> products) {
-        this.id = id;
-        this.date = date;
-        this.products = products;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public LocalDate getDate() {
-        return this.date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public List<Product> getProducts() {
-        return this.products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
+public record Order(@JsonProperty("order_id") Long id,
+                    @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
+                    List<Product> products) {
 
     @JsonProperty("total")
-    @JsonSerialize(using = DoubleSerializer.class)
-    public double getTotal() {
-        double total = 0.0;
-        for (Product product : products) {
-            total += product.getValue();
-        }
-        return total;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Order)) {
-            return false;
-        }
-        Order order = (Order) o;
-        return id == order.id && Objects.equals(date, order.date) && Objects.equals(products, order.products);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, date, products);
+    public BigDecimal getTotal() {
+        return products.stream()
+                       .map(Product::value)
+                       .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
--- src/main/java/com/challenge/models/User.java
+++ src/main/java/com/challenge/models/User.java
@@ -1,65 +1,7 @@
 package com.challenge.models;
 
 import java.util.List;
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"user_id", "name", "orders"})
-public class User {
-
-    @JsonProperty("user_id")
-    private int id;
-    private String name;
-    private List<Order> orders;
-
-    public User() {
-    }
-
-    public User(int id, String name, List<Order> orders) {
-        this.id = id;
-        this.name = name;
-        this.orders = orders;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Order> getOrders() {
-        return this.orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof User)) {
-            return false;
-        }
-        User user = (User) o;
-        return id == user.id && Objects.equals(name, user.name) && Objects.equals(orders, user.orders);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, orders);
-    }
-}
+public record User(@JsonProperty("user_id") Long id, String name, List<Order> orders) {}
